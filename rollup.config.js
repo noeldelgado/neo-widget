bdb49ac4b728f4e0ad90b4bef43e55a023936396
Noel Delgado <auhcsei@gmail.com>
2016-06-08 20:29:06 -0500

    remove / uglify and jsnext:main

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,18 @@
 import buble from 'rollup-plugin-buble';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'index.js',
   format: 'umd',
   moduleName: 'NeoWidget',
   plugins: [
-    nodeResolve({
-      jsnext: true,
+    resolve({
       main: true,
       browser: true,
     }),
     commonjs(),
     buble(),
-    uglify(),
   ],
   dest: './lib/index.js',
 };
